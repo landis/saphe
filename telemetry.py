bfafe63462d205843625599de932f20fcdd157e8
James Landritsi <james.landritsi@gmail.com>
2012-10-12 15:34:28 -0400

    cleanup and comments

--- telemetry.py
+++ telemetry.py
@@ -7,32 +7,40 @@ import pymetar
 import eeml
 from Adafruit.BMP085.Adafruit_BMP085 import BMP085
 
+# ========================
+# Sensor declare
+# ========================
+bmp = BMP085(0x77, 3) # ULTRAHIRES Mode
+DEBUG = 1
+
+# ========================
+# Metar declare
+# ========================
 station = "KPNE"
-LOGGER = 1
-
-API_KEY = '1nmfR0zAp8X0jB27soRr6qCxk_iSAKx5NUJHNGJYajZobz0g'
-FEED = 79880
-API_URL = '/v2/feeds/{feednum}.xml' .format(feednum = FEED)
-
 rf=pymetar.ReportFetcher(station)
 rep=rf.FetchReport()
 rp=pymetar.ReportParser()
 pr=rp.ParseReport(rep)
+locAS = 1013.25
 
-bmp = BMP085(0x77, 3) # ULTRAHIRES Mode
+# ========================
+# COSM declare
+# ========================
+LOGGER = 1
+API_KEY = '1nmfR0zAp8X0jB27soRr6qCxk_iSAKx5NUJHNGJYajZobz0g'
+FEED = 79880
+API_URL = '/v2/feeds/{feednum}.xml' .format(feednum = FEED)
 
-if pr.getPressure() is not None:
-  locAS = pr.getPressure()
-else: 
-  locAS = 1020
+# ========================
+# Setup
+# ========================
 
-header="Timestamp,KPNE Sealevel Pressure (hPa),Pressure (hPa),Celsius,Fahrenheit,Altitude (m)"
-
-print header
-
-print 
+#header="Timestamp,KPNE Sealevel Pressure (hPa),Pressure (hPa),Celsius,Fahrenheit,Altitude (m)"
+#print header
 
 while (True):
+  if pr.getPressure() is not None:
+    locAS = pr.getPressure()
 
   locASP = locAS * 100  
   tmstp = time.strftime("%Y/%m/%d %H:%M:%S")
@@ -45,9 +53,9 @@ while (True):
   altmod = 44330 * (1.0 - pow(pressure/locASP,0.1903))
   altmet = round(altmod, 2)
 
-  lineA = tmstp + "," + str(round(locAS,2)) + "," + str(pressure/100.0) + "," + str(temp) + "," + str(tempF) + "," + str(altmod)
-
-  print lineA
+  if DEBUG:
+    lineA = tmstp + "," + str(round(locAS,2)) + "," + str(pressure/100.0) + "," + str(temp) + "," + str(tempF) + "," + str(altmod)
+    print lineA
   
   if LOGGER:
     pac = eeml.Pachube(API_URL, API_KEY)
@@ -56,4 +64,5 @@ while (True):
     pac.update([eeml.Data(2, pressure)])
     pac.update([eeml.Data(3, altmet)])
     pac.put()
+  
   time.sleep(10)
