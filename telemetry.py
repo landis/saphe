c6f2cea9c8050a362f90f5fd33b34fd55775ab9b
James Landritsi <james.landritsi@gmail.com>
2012-10-12 15:25:11 -0400

    initial commit

---
+++ telemetry.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import sys
+import time
+import datetime
+import pymetar
+import eeml
+from Adafruit.BMP085.Adafruit_BMP085 import BMP085
+
+station = "KPNE"
+LOGGER = 1
+
+API_KEY = '1nmfR0zAp8X0jB27soRr6qCxk_iSAKx5NUJHNGJYajZobz0g'
+FEED = 79880
+API_URL = '/v2/feeds/{feednum}.xml' .format(feednum = FEED)
+
+rf=pymetar.ReportFetcher(station)
+rep=rf.FetchReport()
+rp=pymetar.ReportParser()
+pr=rp.ParseReport(rep)
+
+bmp = BMP085(0x77, 3) # ULTRAHIRES Mode
+
+if pr.getPressure() is not None:
+  locAS = pr.getPressure()
+else: 
+  locAS = 1020
+
+header="Timestamp,KPNE Sealevel Pressure (hPa),Pressure (hPa),Celsius,Fahrenheit,Altitude (m)"
+
+print header
+
+print 
+
+while (True):
+
+  locASP = locAS * 100  
+  tmstp = time.strftime("%Y/%m/%d %H:%M:%S")
+  temp = bmp.readTemperature()
+  tempF = temp * 9 / 5 + 32
+  pressure = bmp.readPressure()
+  altitude = bmp.readAltitude()
+  absaltft = altitude * 3.28084
+  pressInHG = pressure / 3386.38816
+  altmod = 44330 * (1.0 - pow(pressure/locASP,0.1903))
+  altmet = round(altmod, 2)
+
+  lineA = tmstp + "," + str(round(locAS,2)) + "," + str(pressure/100.0) + "," + str(temp) + "," + str(tempF) + "," + str(altmod)
+
+  print lineA
+  
+  if LOGGER:
+    pac = eeml.Pachube(API_URL, API_KEY)
+    pac.update([eeml.Data(0, temp, unit=eeml.Celsius())])
+    pac.update([eeml.Data(1, tempF, unit=eeml.Fahrenheit())])
+    pac.update([eeml.Data(2, pressure)])
+    pac.update([eeml.Data(3, altmet)])
+    pac.put()
+  time.sleep(10)
